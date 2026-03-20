02d2a07b6e938ce769248b6aa49c01d05d354841
Guillermo Velasco <velascoamo.guillermo@gmail.com>
2026-03-20 17:42:59 +0100

    fix: exclude /health endpoint from request logging

--- src/middlewares/requestLogger.ts
+++ src/middlewares/requestLogger.ts
@@ -2,6 +2,8 @@ import type { NextFunction, Request, Response } from "express";
 import logger from "../utils/logger";
 
 export function requestLogger(req: Request, _: Response, next: NextFunction) {
-	logger.info(`${req.method} ${req.originalUrl}`);
+	if (req.path !== "/health") {
+		logger.info(`${req.method} ${req.originalUrl}`);
+	}
 	next();
 }
